7d6154afecb49a310c191942b18a6b3f95d55179
Edward Ramirez <edward.ramirez@gatech.edu>
2025-03-02 03:57:18 -0500

    bring me the robot arm

--- HW3/loadRigData.m
+++ HW3/loadRigData.m
@@ -1,3 +1,7 @@
+forceData = readcell("Block 3 Data Template.xlsx", 'useExcel', true, 'Sheet', 'Part 1');
+forceDataRange = setdiff(4:17, 14);
+forceCalibrationData = cell2mat(forceData(forceDataRange, 2)); % N
+voltageForceCalibrationData = cell2mat(forceData(forceDataRange, 3)); % V
 gainData = readcell("Block 3 Data Template.xlsx", 'useExcel', true, 'Sheet', "Part 2");
 positionCalibrationData = cell2mat(gainData(5:17, 2)); % mm
 voltageCalibrationData = cell2mat(gainData(5:17, 1)); % V
@@ -13,3 +17,4 @@ forceClimbing = cell2mat(climbingSlingData(dataRange, 2));
 save("gainData", "voltageCalibrationData", "positionCalibrationData");
 save("rigDeformationData", "positionRig", "forceRig");
 save("climbingSlingData", "positionClimbing", "forceClimbing");
+save('forceSensorData', 'forceCalibrationData', 'voltageForceCalibrationData');
